ef82584720f7414a8f17c965e3e05078d33ed9b9
susana martinez <maisusy15@outlook.com>
2023-07-13 14:59:00 -0300

    Estructura FrontEnd

---
+++ FrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  //  { path: 'familia', loadChildren: () => import('./familia/familia.module').then(m => m.FamiliaModule)},
+    { path:'',redirectTo:'inicio',pathMatch:'full'},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InterceptorService } from './interceptors/interceptor.service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StylesModule } from './styles.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+@NgModule({
+  declarations: [								
+    AppComponent,
+   ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    StylesModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: InterceptorService,
+    multi: true
+  },],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

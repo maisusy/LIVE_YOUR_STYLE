21bf8e96a444557600bf049b6db5e62a8efa8b12
maisusy <maisusy15@outlook.com>
2023-08-16 12:42:52 -0300

    Fix :Arreglos funcionalidad

--- Backend/Correo/models.py
+++ Backend/Correo/models.py
@@ -3,5 +3,5 @@ from Datos_Usuario.models import Datos_Usuario
 
 # Create your models here.
 class Correo(models.Model):
-    correo : models.CharField(max_length=100)
-    usuario : models.ForeignKey(Datos_Usuario,on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    usuario = models.ForeignKey(Datos_Usuario,on_delete=models.CASCADE)
--- Backend/Correo/serializers.py
+++ Backend/Correo/serializers.py
@@ -3,5 +3,5 @@ from Correo.models import Correo
 
 class CorreoSerializers(serializers.ModelSerializer):
     class Meta:
-        model = Correo,
-        field = ['id','correo','usuario']
+        model = Correo
+        fields = ['id','nombre','usuario']
